e6f6ef3ee053b2424f960e50fff73f26eb774577
Sooyeol Yang <didtnduf2000@gmail.com>
2023-07-31 19:22:48 +0900

    7월 31일

--- 00-pr/post.js
+++ 00-pr/post.js
@@ -1,151 +1,190 @@
-// post 목록 화면을 제작 post.html, post.js
-// fetch api를 사용하여 http://localhost:8080/posts 주소를 호출한 후
+// template: UI형식의 틀
+function createRow(name, phone, email, image) {
+  // 1. 요소 생성
+  const tr = document.createElement("tr");
 
-// 배열 목록을 div(카드) 목록으로 표시한다.
+  // 2. 요소의 속성 설정
+  tr.dataset.email = email;
+  tr.innerHTML = `
+  <td>${name}</td>
+  <td>${phone}</td>
+  <td>${email}</td>  
+  <td>${
+    image
+      ? `<img width="auto" height="30" src="${image}" alt="${name}">`
+      : ""
+  }</td>
+  `;
+  return tr;
+}
 
-//    -----------------              |
-//    | 게시자                        |
-//    | -------------                |
-//    | 1제목(h3)                    |
-//    | 본문(p)                      |
-//    |  .....                       |
-//    |  .....                       |
-//    | -------------                |
-//    | new Date(생성시간).toString() |
-//    -----------------
-//
-//    -----------------
-//    | 게시자         |
-//    | ------------- |
-//    | 2제목(h3)       |
-//    | 본문(p)        |
-//    |  .....        |
-//    |  .....        |
-//    | ------------- |
-//    | 생성시간       |
-//    -----------------
-
+// 데이터 조회 및 목록 생성
 (async () => {
-  const body = document.body;
-  const url = "http://localhost:8080/posts";
-
-  // 1. fetch, 서버에서 데이터 가져오기
-  const response = await fetch(url);
+  const response = await fetch('http://localhost:8080/contacts', { mode: 'no-cors' })
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error('서버 응답이 올바르지 않습니다.');
+    }
+    return response.json();
+    // 응답에 접근할 수 없음
+  })
+  .then(data => {
+    // 성공적으로 JSON 데이터를 받아온 경우 여기서 처리
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error('에러 발생:', error);
+  });
+  // 결과가 배열
   const result = await response.json();
   console.log(result);
 
-  // 배열 메서드를 사용하기 위해서...
-  const data = Array.from(result);
-  console.log(data);
+  const tbody = document.querySelector("tbody");
 
-  // 2.-- 데이터배열 반복문으로 html문자열 만들고,
-  // 컨테이너에 추가
-  data.forEach((item) => {
-    const currentDate = new Date();
-    
-    const template = /*html*/ `
-      <div style="width:300px; margin-bottom:3rem;" data-no="${
-        item.no
-      }">
-        <em>${item.creatorName}</em>
-        <hr>
-        <h3>${item.title}</h3>
-        <h1>${item.creatorName}</h1>
-        <p>${item.content}</p>
-        <hr>
-        <small>${item.createdTime}</small>
-      </div>
-    `;
-
-    body.insertAdjacentHTML(
-      "beforeend",
-      template
+  // 배열 반복을 해서 tr만든다음에 tbody 가장 마지막 자식에 추가
+  for (let item of result) {
+    tbody.append(
+      createRow(
+        item.name,
+        item.phone,
+        item.email,
+        item.image
+      )
     );
-  });
+  }
 })();
-// ------------------------------------------------------------------------
+
+// 추가폼 처리
 (() => {
   const form = document.forms[0];
   const inputs = form.querySelectorAll("input");
-  const textareas = form.querySelectorAll("textarea")
-  
 
-  const title = inputs[0];
-  const creatorName = inputs[1];
-  const content = textareas[0];
+  const name = inputs[0];
+  const phone = inputs[1];
+  const email = inputs[2];
+  const file = inputs[3]; // input type="file"
 
   const add = form.querySelector("button");
 
-  add.addEventListener("click", async (e) => {
+  add.addEventListener("click", (e) => {
     e.preventDefault();
 
-    if (title.value === "") {
-      alert("제목을 입력해주세요.");
+    if (email.value === "") {
+      alert("이메일을 입력해주세요.");
       return;
     }
 
-    if (content.value === "") {
-      alert("내용을 입력해주세요.");
+    if (name.value === "") {
+      alert("이름을 입력해주세요.");
       return;
     }
 
-    if (creatorName.value === "") {
-      alert("작성자명을 입력해주세요.");
+    if (phone.value === "") {
+      alert("전화번호를 입력해주세요.");
       return;
     }
 
-    const tbody = document.querySelector("tbody");
-    const tr = document.createElement("tr");
-    // 삭제할 때 사용하려고 데이터 속성을 추가함
-    tr.dataset.title = title.value;
+    // 데이터를 서버에 전송하고, UI요소 생성
+    async function createContact(image) {
+      /// --- 서버전송하면 UI 생성
 
-    tr.innerHTML = `
-    <td>
-      ${title.value}
-    </td>
-    <td>
-      ${creatorName.value}
-    </td>
-    <td>
-      ${content.value}
-    </td>
-    tbody.prepend(tr);
-    form.reset();
-    </td>`;
-  
-  const response = await fetch(
-    "http://localhost:8080/posts",
-    {
-      // HTTP Method
-      method: "POST",
-      // 보낼 데이터 형식은 json
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify({
-        title: title.value,
-        content: content.value,
-        creatorName: creatorName.value,
-      }),
+      // 서버에 데이터를 전송
+      // fetch(url, options)
+      const response = await fetch(
+        "http://localhost:8080/contacts",
+        {
+          // HTTP Method
+          method: "POST",
+          // 보낼 데이터 형식은 json
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify({
+            email: email.value,
+            name: name.value,
+            phone: phone.value,
+            image: image ? image : null,
+          }),
+        }
+      );
+      console.log(response);
+
+      const result = await response.json();
+      console.log(result);
+
+      // 화면에 요소를 추가하는 것은 데이처리가 정상적으로 된 다음에
+      // 서버에서 응답받은 데이터
+      const { data } = result;
+
+      // --- 3. 어딘가(부모, 다른요소)에 추가한다(append, prepend);
+      document
+        .querySelector("tbody")
+        .prepend(
+          createRow(
+            data.name,
+            data.phone,
+            data.email,
+            data.image
+          )
+        );
+      form.reset();
     }
-  );
-  console.log(response);
 
-  const result = await response.json();
-  console.log(result);
+    if (file.files[0]) {
+      // 파일이 있을 때
+      const reader = new FileReader();
+      // reader로 파일을 읽기가 완료되면 실행되면 이벤트 핸들러 함수
+      reader.addEventListener(
+        "load",
+        async (e) => {
+          console.log(e);
+          // file -> base64 data-url
+          const image = e.target.result;
+          createContact(image);
+        }
+      );
+      // 파일을 dataURL(base64)로 읽음
+      reader.readAsDataURL(file.files[0]);
+    } else {
+      // 파일이 없을 때
+      createContact();
+    }
 
-  // 화면에 요소를 추가하는 것은 데이처리가 정상적으로 된 다음에
+    // return;
+  });
 
-  // --- 3. 어딘가(부모, 다른요소)에 추가한다(append, prepend);
-  
-  // tbody.prepend(
-    
-  //     title.value,
-  //     content.value,
-  //     creatorName.value
-    
-  // );
-  form.reset();
-});
   console.log("추가폼 처리 코드");
+})();
+
+// 삭제폼 처리
+(() => {
+  const form = document.forms[1];
+
+  const email = form.querySelector("input");
+  const del = form.querySelector("button");
+
+  del.addEventListener("click", async (e) => {
+    e.preventDefault();
+
+    // 서버통신
+    await fetch(
+      `http://localhost:8080/contacts/${email.value}`,
+      {
+        method: "DELETE",
+      }
+    );
+
+    const tr = document.querySelector(
+      `tr[data-email="${email.value}"]`
+    );
+
+    if (!tr) {
+      alert("해당 이메일의 연락처 없습니다.");
+      return;
+    }
+
+    tr.remove();
+
+    form.reset();
+  });
 })();
